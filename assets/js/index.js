6ab00044d9da5b7b01d86e024db9ff2d4f42a65a
Franco Lorca <lorcafrancog@proton.me>
2023-04-02 16:24:31 -0300

    📝 Update(index.html): Change variables name, remove DOMContentLoad listener and change btnDice listener

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
-const $adviceId = document.querySelector(".advice__id")
-const $adviceQuote = document.querySelector(".advice__quote")
-const $btnDice = document.querySelector(".btn__dice")
+
+const adviceId = document.querySelector(".advice__id")
+const adviceQuote = document.querySelector(".advice__quote")
+const btnDice = document.querySelector(".btn__dice")
 
 // Get quote
 const getQuote = async () => {
@@ -13,18 +14,14 @@ const getQuote = async () => {
 
 // Set Id and Quote
 const setIdAndQuote = async () => {
-  const data = await getQuote();
+  const response = await getQuote();
   
-  $adviceId.textContent = "ADVICE #" + data.data.slip.id
-  $adviceQuote.textContent = `"${data.data.slip.advice}"`
+  adviceId.textContent = "ADVICE #" + response.data.slip.id
+  adviceQuote.textContent = `"${response.data.slip.advice}"`
 }
 
 // Get and set quote on start
-document.addEventListener("DOMContentLoaded", () => {
-   setIdAndQuote()
-})
+setIdAndQuote();
 
 // Get and set new quote when click button
-$btnDice.addEventListener("click", (e) => {
-  setIdAndQuote()
-})
+btnDice.addEventListener("click", setIdAndQuote)
